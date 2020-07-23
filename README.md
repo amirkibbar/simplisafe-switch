af45cdbbf889970587eeaf3975823c862fdff3ef
amir-kibbar <amir.kibbar@gmail.com>
2020-07-23 16:47:23 -0700

    replaced with a python script, because the MFA was too difficult to figure out on my own

--- README.md
+++ README.md
@@ -2,36 +2,19 @@
 
 Turn on or off simplisafe.com alarm system. To use this you must have the upgraded monitoring package.
 
-The script assumes that you want to set the same state to all your subscriptions.
+The script assumes that you want to set the state to your first subscription only.
 
 To use it create an authorization file in the following format:
 
 ```json
 {
-  "grant_type": "password",
   "username": "your-simplisafe-user",
-  "password": "your-simplisafe-pass"
+  "password": "your-simplisafe-pass",
+  "client_id": "uuid"
 }
 ```
 
-The `login.sh`, and `get-state.sh` scripts output machine-parsable results.
+This script is based on https://github.com/bachya/simplisafe-python, see instructions there on how to
+obtain the MFA client_id, specifically these instructions here: https://simplisafe-python.readthedocs.io/en/latest/usage.html#simplisafe-multi-factor-authentication.
 
-Use the `set-state.sh` to change the state of the alarm. The script will only change from `AWAY` state when you pass
-the `--force` option.
-
-Examples:
-
-Set state to home:
-
-```bash
-./set-state.sh auth.json HOME
-```
-
-Set state to off even if it's away:
-
-```bash
-./set-state.sh auth.json OFF --force
-```
-
-The API is partly reversed engineered from the simplisafe.com dashboard, and partly based on this thread:
-https://community.smartthings.com/t/simplisafe-alarm-integration-cloud-to-cloud/8473/44.
+I recommend using a virtual env with this script. The minimum Python version is 3.7, I used 3.8.4.
---
+++ simpli.py
@@ -0,0 +1,76 @@
+import asyncio
+import click
+import json
+
+from aiohttp import ClientSession
+import simplipy
+
+
+async def get_system():
+  creds = get_creds()
+  async with ClientSession() as session:
+    simplisafe = await simplipy.API.login_via_credentials(creds["username"], creds["password"], client_id=creds["client_id"], session=session)
+    systems = await simplisafe.get_systems()
+    
+    system = systems.get(list(systems.keys())[0])
+    return system
+
+
+def get_creds():
+  with open('./simplisafe.json') as f:
+    return json.load(f)
+
+
+async def turn_system_on(mode):
+  system = await get_system()
+  curr_state = system.state
+  print('SimpliSafe at {}'.format(system.address))
+
+  if curr_state == simplipy.system.SystemStates.off:
+    print('System is OFF, turning it on to mode {}'.format(mode))
+    if mode == "home":
+      await system.set_home()
+    elif mode == "away":
+      await system.set_away()
+    else:
+      print('Unknown mode {}'.format(mode))
+    
+    system = await get_system()
+    print('SimpliSafe state is now {}'.format(system.state))
+  else:
+    print('System is {}, aborting turn on request to mode {}'.format(curr_state, mode))
+
+
+async def turn_system_off():
+  system = await get_system()
+  curr_state = system.state
+  print('SimpliSafe at {}'.format(system.address))
+
+  if curr_state == simplipy.system.SystemStates.home:
+    print('System is turned on and in more HOME, turning it off')
+    await system.set_off()
+
+    system = await get_system()
+    print('SimpliSafe state is now {}'.format(system.state))
+  else:
+    print('System is in mode {}, ignoring turn off request'.format(curr_state))
+
+
+@click.group()
+def cli():
+  pass
+
+
+@cli.command()
+@click.option("--mode", default="home", help="home or away")
+def turn_on(mode):
+  asyncio.run(turn_system_on(mode))
+
+
+@cli.command()
+def turn_off():
+  asyncio.run(turn_system_off())
+
+
+if __name__ == '__main__':
+  cli()

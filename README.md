557c08aff6d7fbc762c13ff2623ba82c9e19777a
Amir Kibbar <amir.kibbar@tri.global>
2022-02-27 15:28:08 -0800

    2022 refresh

--- README.md
+++ README.md
@@ -4,17 +4,19 @@ Turn on or off simplisafe.com alarm system. To use this you must have the upgrad
 
 The script assumes that you want to set the state to your first subscription only.
 
-To use it create an authorization file in the following format:
+This script is based on https://github.com/bachya/simplisafe-python, see instructions there on how to obtain the
+verification codes: https://simplisafe-python.readthedocs.io/en/latest/usage.html#installation. Once you have codes run
+this once:
 
-```json
-{
-  "username": "your-simplisafe-user",
-  "password": "your-simplisafe-pass",
-  "client_id": "uuid"
-}
+```bash
+python3 simpli.py authenticate --code <authentication-code> --verifier <verifier-code>
 ```
 
-This script is based on https://github.com/bachya/simplisafe-python, see instructions there on how to
-obtain the MFA client_id, specifically these instructions here: https://simplisafe-python.readthedocs.io/en/latest/usage.html#simplisafe-multi-factor-authentication.
+Again, both `authentication-code` and `verifier-code` should be obtained by following the instructions in the [
+`simplisafe-python` library](https://simplisafe-python.readthedocs.io/en/latest/usage.html#installation).
+
+This will create a local `.simplisafe-auth.json` with the authentication and refresh tokens. Treat this file as a
+password file and protect it. Every time you run the `simpli.py` and connect to the SimpliSafe servers, this file will
+be updated with new tokens.
 
 I recommend using a virtual env with this script. The minimum Python version is 3.7, I used 3.8.4.
--- simpli.py
+++ simpli.py
@@ -1,97 +1,130 @@
 import asyncio
-import click
 import json
+from types import SimpleNamespace
 
-from aiohttp import ClientSession
+import click
 import simplipy
+from aiohttp import ClientSession
+
+
+def store_creds(api):
+    token = {
+        'access_token': api.access_token,
+        'refresh_token': api.refresh_token,
+        'user_id': api.user_id,
+    }
+    with open('.simplisafe-auth.json', 'w') as out:
+        json.dump(token, out)        
+
+
+async def get_system_from_session(session):
+    creds = get_creds()
+    api = await simplipy.API.async_from_refresh_token(creds.refresh_token, session=session)
+    store_creds(api)
+    systems = await api.async_get_systems()
+
+    return systems.get(list(systems.keys())[0])
 
 
 async def get_system():
-  creds = get_creds()
-  async with ClientSession() as session:
-    simplisafe = await simplipy.API.login_via_credentials(creds["username"], creds["password"], client_id=creds["client_id"], session=session)
-    systems = await simplisafe.get_systems()
-    
-    system = systems.get(list(systems.keys())[0])
-    return system
+    async with ClientSession() as session:
+        return await get_system_from_session(session)
 
 
 def get_creds():
-  with open('./simplisafe.json') as f:
-    return json.load(f)
+    with open("./.simplisafe-auth.json") as f:
+        return json.loads(f.read(), object_hook=lambda d: SimpleNamespace(**d))
 
 
 async def turn_system_on(mode):
-  system = await get_system()
-  curr_state = system.state
-  print('SimpliSafe at {}'.format(system.address))
+    system = await get_system()
+    curr_state = system.state
+    print("SimpliSafe at {}".format(system.address))
 
-  if curr_state == simplipy.system.SystemStates.off:
-    print('System is OFF, turning it on to mode {}'.format(mode))
-    if mode == "home":
-      await system.set_home()
-    elif mode == "away":
-      await system.set_away()
+    if curr_state == simplipy.system.SystemStates.OFF:
+        print("System is OFF, turning it on to mode {}".format(mode))
+        async with ClientSession() as session:
+            system = await get_system_from_session(session)
+            if mode == "home":
+                await system.async_set_home()
+            elif mode == "away":
+                await system.async_set_away()
+            else:
+                print("Unknown mode {}".format(mode))
+
+        system = await get_system()
+        print("SimpliSafe state is now {}".format(system.state))
     else:
-      print('Unknown mode {}'.format(mode))
-    
+        print("System is {}, aborting turn on request to mode {}".format(curr_state, mode))
+
+
+async def turn_system_off() -> None:
     system = await get_system()
-    print('SimpliSafe state is now {}'.format(system.state))
-  else:
-    print('System is {}, aborting turn on request to mode {}'.format(curr_state, mode))
+    curr_state = system.state
+    print("SimpliSafe at {}".format(system.address))
+
+    if curr_state == simplipy.system.SystemStates.HOME:
+        async with ClientSession() as session:
+            system = await get_system_from_session(session)
+            print("System is turned on and in more HOME, turning it off")
+            await system.async_set_off()
+
+            system = await get_system()
+            print("SimpliSafe state is now {}".format(system.state))
+    else:
+        print("System is in mode {}, ignoring turn off request".format(curr_state))
 
 
-async def turn_system_off():
-  system = await get_system()
-  curr_state = system.state
-  print('SimpliSafe at {}'.format(system.address))
+async def print_sensors_status() -> None:
+    async with ClientSession() as session:
+        system = await get_system_from_session(session)
+        for serial, sensor in system.sensors.items():
+            await sensor.async_update(cached=False)
+            if sensor.type.name != "keypad" and sensor.type.name != "keychain":
+                status = "closed"
+                if sensor.triggered and sensor.type.name == "entry":
+                    status = "open"
+                elif sensor.triggered:
+                    status = "triggered"
+                elif sensor.type.name != "entry":
+                    status = "not triggered"
 
-  if curr_state == simplipy.system.SystemStates.home:
-    print('System is turned on and in more HOME, turning it off')
-    await system.set_off()
-
-    system = await get_system()
-    print('SimpliSafe state is now {}'.format(system.state))
-  else:
-    print('System is in mode {}, ignoring turn off request'.format(curr_state))
+                print("Sensor {} ({}) is {}".format(sensor.name, sensor.type.name, status))
 
 
-async def print_sensors_status():
-  system = await get_system()
-  for serial, sensor in system.sensors.items():
-    await sensor.update(cached=True)
-    if sensor.type.name != 'keypad' and sensor.type.name != 'keychain':
-      status = 'closed'
-      if sensor.triggered and sensor.type.name == 'entry':
-        status = 'open'  
-      elif sensor.triggered:
-        status = 'triggered'
-      elif sensor.type.name != 'entry':
-        status = "not triggered"
-
-      print('Sensor {} ({}) is {}'.format(sensor.name, sensor.type.name, status))
+async def generate_code(auth_code, verifier_code) -> None:
+    async with ClientSession() as session:
+        api = await simplipy.API.async_from_auth(auth_code, verifier_code, session=session)
+        store_creds(api)
 
 
 @click.group()
 def cli():
-  pass
+    pass
 
 
 @cli.command()
 @click.option("--mode", default="home", help="home or away")
 def turn_on(mode):
-  asyncio.run(turn_system_on(mode))
+    asyncio.run(turn_system_on(mode))
 
 
 @cli.command()
 def turn_off():
-  asyncio.run(turn_system_off())
+    asyncio.run(turn_system_off())
 
 
 @cli.command()
 def sensors_status():
-  asyncio.run(print_sensors_status())
+    asyncio.run(print_sensors_status())
 
 
-if __name__ == '__main__':
-  cli()
+@cli.command()
+@click.option("--code", help="authorization code")
+@click.option("--verifier", help="verifier code")
+def authenticate(code, verifier):
+    asyncio.run(generate_code(code, verifier))
+
+
+if __name__ == "__main__":
+    cli()
